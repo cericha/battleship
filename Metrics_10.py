9067027ff13608b30eda7c2e27f63e18bdff2704
Charles Richards <charleserichards@gmail.com>
2024-04-24 14:55:09 -0400

    Add type support

--- Metrics_10.py
+++ Metrics_10.py
@@ -2,6 +2,6 @@ class Metrics:
     """
     Contains information that we want to collect about an instance of playing the game
     """
-    def __init__(self, time, moves):
+    def __init__(self, time: float, moves: int) -> None:
         self.time = time
         self.moves = moves
